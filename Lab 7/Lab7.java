4e1c7542663018404b01e3d384dc2775da60c315
George Maxwell <1641870@brunel.ac.uk>
2018-11-26 15:18:25 +0000

    finished lab 8 and small modification to lab 7

--- Lab 7/Lab7.java
+++ Lab 7/Lab7.java
@@ -3,17 +3,16 @@ import java.util.Random;
 
 public class Lab7 {
 	public static void main(String[] args) {
-		//double [] [] g={{0,1,2,3,3},{1,0,3,4,4},{2,3,0,4,1},{3,4,4,0,2},{3,4,1,2,0} };
+		double [] [] g={{0,1,2,3,3},{1,0,3,4,4},{2,3,0,4,1},{3,4,4,0,2},{3,4,1,2,0} };
 		double startTime = System.currentTimeMillis();
-		double [] [] g = randomArray(500);
 		double [][] mst = MST.PrimsMST(g);
 		double endtime = System.currentTimeMillis();
-		//printArray(mst);
 		double totalTime = endtime - startTime;
 		System.out.println("total time taken = " + totalTime + " milliseconds");
 		
 	}
 	
+	//print the 2-D array
 	static public void printArray(double [] [] mst) {
 		DecimalFormat decimalFormat = new DecimalFormat();
 		for (int i = 0; i < mst.length; i++) {
@@ -24,6 +23,8 @@ public class Lab7 {
 			System.out.println("]");			
 		}
 	}
+	
+	//Generate random array of size n
 	static double[][] randomArray(int n){
 		Random random = new Random();
 		double [][] randomArray = new double[n][n];
--- Lab 8/Lab8.java
+++ Lab 8/Lab8.java
@@ -3,18 +3,21 @@ import java.util.ArrayList;
 public class Lab8 {
 	public static void main(String[] args) {
 		ArrayList<Double> weights = new ArrayList<>();
-		ScalesSolution s = new ScalesSolution("00001");
-		weights.add(1.0);
-		weights.add(2.0);
-		weights.add(3.0);
-		weights.add(4.0);
-		weights.add(10.0);
+		
+		String path = System.getProperty("user.dir") + "\\src\\1000 Primes.txt";
+		ScalesSolution s = new ScalesSolution(1000);
+		int counter =0;
+		weights.addAll(CS2004.ReadNumberFile( path));
+		while (s.ScalesFitness(weights)> 1000) {
+			s.println();
+			System.out.println(s.ScalesFitness(weights));
+			s= new ScalesSolution(1000);
+			counter ++;
+		}
+		
 		s.println();
 		//s.print();
 		System.out.println(s.ScalesFitness(weights));
-
-		
-
-
+		System.out.println("It took:" + counter + " times");
 	}
 }
